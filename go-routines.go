2aed6294458587cadd4f4d66818d2c5291991d25
AdonaIsium <michael.tau21@gmail.com>
2025-01-26 18:38:59 -0600

    added some channels to send and receive on.

--- go-routines.go
+++ go-routines.go
@@ -5,17 +5,46 @@ import (
 	"sync"
 )
 
-func reader(id int, wg *sync.WaitGroup) {
+func reader(id int, ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println("Hello ")
+
+	for{
+		val, ok := <-ch
+		if !ok {
+			fmt.Printf("%d 다닸어요\n", id)
+			return
+		}
+		fmt.Printf("%d는 %d바닸어요\n", id, val)
+	}
+
+	
 }
 
 func main() {
 	var wg sync.WaitGroup
+	ch := make(chan int)
+
 	wg.Add(2)
 
-	go reader(&wg)
-	go reader(&wg)
+	go reader(1, ch, &wg)
+	go reader(2, ch, &wg)
+	go reader(3, ch, &wg)
+	go reader(4, ch, &wg)
+	go reader(5, ch, &wg)
+	go reader(6, ch, &wg)
+	go reader(7, ch, &wg)
+	go reader(8, ch, &wg)
+	go reader(9, ch, &wg)
+	go reader(10, ch, &wg)
+	go reader(11, ch, &wg)
+	go reader(12, ch, &wg)
+
+	for i := 0; i < 1000; i++ {
+		ch <- i
+	}
+
+
+	close(ch)
 
 	wg.Wait()
 }
